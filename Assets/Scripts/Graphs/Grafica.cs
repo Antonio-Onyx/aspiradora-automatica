67b1277944223d8af8f4d09bcddb032df5c7e7da
Antonio-Onyx <ant0ni0lnx31@gmail.com>
2024-03-16 22:11:39 -0600

    complete la funcion Astar()

--- Assets/Scripts/Graphs/Grafica.cs
+++ Assets/Scripts/Graphs/Grafica.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Grafica{
@@ -7,10 +9,7 @@ public class Grafica{
     public List<Vertice> grafica = new List<Vertice>();
 	public List<Vertice> camino = new List<Vertice>();
 	private Vertice verticeActual, verticeDestino;
-
-	private List<Vertice> abiertos = new List<Vertice>(); //Lista de nodos que aun no se han explorado
-	private List<Vertice> cerrados = new List<Vertice>(); //Lista de nodos ya explorados
-
+	
 	//Agrega un v�rtice a la lista de v�rtices de la gr�fica.
     public void AgregarVertice(Vertice nuevoVertice) {
         if(!grafica.Contains(nuevoVertice)){
@@ -21,7 +20,45 @@ public class Grafica{
 	//Aplica el Algoritmo de A*
 	public bool AStar(Vertice inicio, Vertice final) {
 		//Completar
-		return true;
+		List<Vertice> abiertos = new List<Vertice>(); //Lista de nodos que aun no se han explorado
+		List<Vertice> cerrados = new List<Vertice>(); //Lista de nodos ya explorados
+
+		abiertos.Add(inicio);
+
+		while(abiertos.Count > 0){ //mientras la lista de abiertos no sea vacia / mientras no hayamos explorado todo
+			Vertice actual;
+			actual = abiertos[0];
+			if(actual == final){
+				reconstruirCamino(inicio, final);
+				return true;
+			}
+			abiertos.RemoveAt(0);
+			cerrados.Add(actual);
+
+			foreach(Vertice vecino in actual.vecinos){
+				if(cerrados.Contains(vecino)){
+					continue;
+				}
+
+				//f(n) = g(n) + h(n)
+				//g(n)
+				float costoG = actual.g + distancia(actual, vecino);
+
+				if(!abiertos.Contains(vecino) || costoG < vecino.g){
+					vecino.padre = actual;
+					vecino.g = costoG;
+					vecino.h = distancia(vecino, final);
+					vecino.f = vecino.g + vecino.h;
+
+					if(!abiertos.Contains(vecino)){
+						abiertos.Add(vecino);
+					}
+				}
+			}
+
+		}
+
+		return false;
     }
 
 	//Auxiliar que reconstruye el camino de A*
@@ -32,6 +69,7 @@ public class Grafica{
 		verticeActual = final.camino;
 		while(verticeActual.id !=  inicio.id){
 			camino.Add(verticeActual);
+			verticeActual = verticeActual.camino;
 		}
 		camino.Add(inicio); 
 	}
@@ -44,15 +82,13 @@ public class Grafica{
 	int menorF(List<Vertice> l) {
 		//Coompletar
 		float menorValor = l[0].f;
-		int contador = 0;
 		int indice = 0;
 
 		for(int i = 0; i < l.Count; i++){
 			if(l[i].f < menorValor){
 				menorValor = l[i].f;
-				indice = contador;
+				indice = i;
 			}
-			contador++;
 		}
 		return indice;
 	}
